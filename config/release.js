6455d0290edac368d1fa27a73a466e6f3b3ce47b
Steven Lindberg <stevenjohnlindberg@gmail.com>
2016-01-16 10:47:54 -0800

    Use dates for versions

--- config/release.js
+++ config/release.js
@@ -3,14 +3,9 @@
 
 // For details on each option run `ember help release`
 module.exports = {
-  // local: true,
-  // remote: 'some_remote',
-  // annotation: "Release %@",
-  // message: "Bumped version to %@",
-  // manifest: [ 'package.json', 'bower.json', 'someconfig.json' ],
-  // strategy: 'date',
-  // format: 'YYYY-MM-DD',
-  // timezone: 'America/Los_Angeles',
+  message: "Bumped version to %@",
+  strategy: 'date',
+  format: 'YYYY-MM-DD',
   //
   // beforeCommit: function(project, versions) {
   //   return new RSVP.Promise(function(resolve, reject) {
